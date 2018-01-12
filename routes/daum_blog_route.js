12d8c75ef3fad80d5eebdf4bc26b1e63c7ea9c31
kyungjoongo <kyungjoon.go>
2018-01-12 23:04:10 +0900

    afsdfsdfsafddsfgsdfgasdfsdfasdf

--- routes/daum_blog_route.js
+++ routes/daum_blog_route.js
@@ -10,11 +10,11 @@ var querystring = require('querystring');
 router.get('/blog_list', function (req, _response, next) {
 
 
-
     var page = req.query.page;
     var query = req.query.query
 
-    var encodedQuery = querystring.escape(query);;
+    var encodedQuery = querystring.escape(query);
+    ;
 
     request({
         headers: {
@@ -22,7 +22,7 @@ router.get('/blog_list', function (req, _response, next) {
             , 'Content-Type': 'application/json;charset=UTF-8'
         },
         encoding: null,
-        uri: 'https://dapi.kakao.com/v2/search/blog?query='+ encodedQuery+ '&page='+ page +  '&size=20',
+        uri: 'https://dapi.kakao.com/v2/search/blog?query=' + encodedQuery + '&page=' + page + '&size=20',
         method: 'GET'
     }, function (err, res, body) {
         //it works!
@@ -36,12 +36,12 @@ router.get('/blog_list', function (req, _response, next) {
 
             var __url = jsonArray.documents[i].url;
 
-/*
-            var res = syncRequest('get', __url);
-            var $ = cheerio.load(res.getBody());
-            var image = $('.imageblock').first().children().attr('src');
+            /*
+                        var res = syncRequest('get', __url);
+                        var $ = cheerio.load(res.getBody());
+                        var image = $('.imageblock').first().children().attr('src');
 
-            jsonArray.documents[i].datetime = image;*/
+                        jsonArray.documents[i].datetime = image;*/
 
         }
 
@@ -52,6 +52,37 @@ router.get('/blog_list', function (req, _response, next) {
          _response.end(jsonArray.documents);*/
 
         _response.json(jsonArray.documents)
+    });
+
+
+});
+
+
+router.get('/blog_list2', function (req, _response, next) {
+
+
+    var page = req.query.page;
+    var query = req.query.query
+
+    var encodedQuery = querystring.escape(query);
+    ;
+
+    request({
+        headers: {
+            'Authorization': 'KakaoAK 28449fe1535e7f4f2d0d605b5a1af7a6'
+            , 'Content-Type': 'application/json;charset=UTF-8'
+        },
+        encoding: null,
+        uri: 'https://dapi.kakao.com/v2/search/blog?query=' + encodedQuery + '&page=' + page + '&size=20',
+        method: 'GET'
+    }, function (err, res, body) {
+        //it works!
+
+        _response.writeHead(200, {'Content-Type': 'application/json;charset=UTF-8'});
+
+        _response.end(body);
+
+
     });
 
 
