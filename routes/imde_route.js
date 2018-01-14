691608eb8c38194cd1c1a420d650ec909d37c684
kyungjoongo <kyungjoon.go>
2018-01-14 14:06:09 +0900

    afsdfsdfsafddsfgsdfgasdfsdfasdf

--- routes/imde_route.js
+++ routes/imde_route.js
@@ -23,7 +23,7 @@ router.get('/imde_info_list/', function (req, last_response, next) {
 
     var querystring = require('querystring');
     var encodedQuery = querystring.escape('임대주택 청약 정보');
-    var api_url = 'https://openapi.naver.com/v1/search/blog?query='+ encodedQuery+ '&start='+ start+ '&display='+ displaySize; // json 결과
+    var api_url = 'https://openapi.naver.com/v1/search/blog?query='+ encodedQuery+ '&start='+ start+ '&display='+ displaySize+ '&sort=date'; // json 결과
     var striptags = require('striptags');
     var prettyjson = require('prettyjson');
 
@@ -48,8 +48,17 @@ router.get('/imde_info_list/', function (req, last_response, next) {
             var link = blogArrray.items[i].link.replace('&amp;', '&')
 
 
+            var postdate = blogArrray.items[i].postdate
+
+            var year= postdate.substring(0,4)
+            var month= postdate.substring(4,6)
+            var date= postdate.substring(6,8)
+
+            var _date = year+"-"+ month+ "-"+  date;
+
             blogArrray.items[i].title = title;
-            blogArrray.items[i].link = link;
+            blogArrray.items[i].title = title;
+            blogArrray.items[i].date = _date;
 
         }
 
