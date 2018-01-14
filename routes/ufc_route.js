e0f9f2aa3673ff2b19957208dbaa36cb2ff88eaf
kyungjoongo <kyungjoon.go>
2018-01-14 15:26:35 +0900

    afsdfsdfsafddsfgsdfgasdfsdfasdf

--- routes/ufc_route.js
+++ routes/ufc_route.js
@@ -10,7 +10,6 @@ var syncRequest = require('sync-request');
 router.get('/title_holder', function (req, _response, next) {
 
 
-
     var res = syncRequest('GET', 'http://www.ufc.com/fighter/');
     var $ = cheerio.load(res.getBody());
 
@@ -43,9 +42,9 @@ router.get('/title_holder', function (req, _response, next) {
 
         fianlresultJson.push({
             fighter_name: fighter_name,
-            weight_class : weight_class,
-            fighter_image: 'https://'+ fighter_image,
-            fighters_wld:fighters_wld
+            weight_class: weight_class,
+            fighter_image: 'https://' + fighter_image,
+            fighters_wld: fighters_wld
         })
 
 
@@ -54,7 +53,6 @@ router.get('/title_holder', function (req, _response, next) {
     _response.json({result: fianlresultJson});
 
 });
-
 
 
 router.get('/event_list', function (req, _response, next) {
@@ -72,14 +70,14 @@ router.get('/event_list', function (req, _response, next) {
 //    console.log('http://kr.ufc.com' + fight_title);
 
         var url = 'http://kr.ufc.com' + fight_title;
-        fight_title  = fight_title.replace('/event/', '').replace('#/fight', '');
+        fight_title = fight_title.replace('/event/', '').replace('#/fight', '');
 
 
         var imageLeft = $(this).parent().parent().next().children().find('.frame-left').children().children('img').attr('src');
         var imageRight = $(this).parent().parent().next().children().find('.frame-right').children().children('img').attr('src');
 
-        imageLeft ='http://'+  imageLeft.substring(2,imageLeft.length)
-        imageRight ='http://'+  imageRight.substring(2,imageRight.length)
+        imageLeft = 'http://' + imageLeft.substring(2, imageLeft.length)
+        imageRight = 'http://' + imageRight.substring(2, imageRight.length)
 
         fianlresultJson.push({
 
@@ -99,20 +97,17 @@ router.get('/event_list', function (req, _response, next) {
 });
 
 
-
-
 router.get('/fightcard_detail2/:title', function (req, _response, next) {
 
     var title = req.params.title;
 
     /*http://www.ufc.com/event/UFC-220?f1=Stipe-Miocic&f2=francis-ngannou*/
 
-    var res = syncRequest('GET', 'http://www.ufc.com/event/'+ title);
+    var res = syncRequest('GET', 'http://www.ufc.com/event/' + title);
 
     /*http://www.ufc.com/event/UFC-220#/fight*/
-/*
-    http://www.ufc.com/event/UFC-220*/
-
+    /*
+        http://www.ufc.com/event/UFC-220*/
 
 
     var $ = cheerio.load(res.getBody());
@@ -120,7 +115,7 @@ router.get('/fightcard_detail2/:title', function (req, _response, next) {
     var index = 1;
 
 
-    var title= ''
+    var title = ''
 
     $('#titleArea').each(function () {
 
@@ -144,38 +139,46 @@ router.get('/fightcard_detail2/:title', function (req, _response, next) {
         figher_name2 = figher_name2.replace("WIN", "").replace('**', '').trim();
 
         var fighterfirstname = $(this).find('.fighter-first-name').html();
-        console.log('fighterfirstname'+ fighterfirstname);
+        console.log('fighterfirstname' + fighterfirstname);
+
+
+        var fighter_names1 = ''
+        var extract_fighter_name1 = ''
+        var fighter_names2 = ''
+        var extract_fighter_name2 = ''
+
+        if (fighter_image1 != undefined && fighter_image1 != '') {
+
+            fighter_names1 = fighter_image1.split("/")
+            extract_fighter_name1 = fighter_names1[5];
+            fighter_names2 = fighter_image2.split("/")
+            extract_fighter_name2 = fighter_names2[5];
+            console.log("###-->" + extract_fighter_name1 + "###" + extract_fighter_name2);
+
+            fighter_image1 = 'http://' + fighter_image1.substring(2, fighter_image1.length)
+            fighter_image2 = 'http://' + fighter_image2.substring(2, fighter_image2.length)
+
+            console.log("####" + fighter_image1);
+            console.log("####" + figher_name2);
+
+            imageListArray.push({
+                fighter_image1: fighter_image1,
+                fighter_image2: fighter_image2,
+                figher_name1: figher_name1,
+                figher_name2: figher_name2,
+                extract_fighter_name1: extract_fighter_name1,
+                extract_fighter_name2: extract_fighter_name2
+            })
+        }
 
 
 
-        var fighter_names1= fighter_image1.split("/")
-        var extract_fighter_name1 =  fighter_names1[5];
-
-        var fighter_names2= fighter_image2.split("/")
-        var extract_fighter_name2 =  fighter_names2[5];
-
-        console.log("###-->" + extract_fighter_name1 +"###"+  extract_fighter_name2 );
-
-        fighter_image1 ='http://'+  fighter_image1.substring(2,fighter_image1.length)
-        fighter_image2 ='http://'+  fighter_image2.substring(2,fighter_image2.length)
-
-        console.log("####"+ fighter_image1);
-        console.log("####"+ figher_name2);
-
-        imageListArray.push({
-            fighter_image1: fighter_image1,
-            fighter_image2: fighter_image2,
-            figher_name1 : figher_name1,
-            figher_name2 : figher_name2,
-            extract_fighter_name1 : extract_fighter_name1,
-            extract_fighter_name2 : extract_fighter_name2
-        })
 
     });
 
     fianlresultJson.push({
         title: title,
-        fight_images :imageListArray
+        fight_images: imageListArray
 
     })
 
@@ -186,15 +189,13 @@ router.get('/fightcard_detail2/:title', function (req, _response, next) {
 });
 
 
-
-
 router.get('/fightcard_detail', function (req, _response, next) {
 
 
     var res = syncRequest('GET', 'http://www.ufc.com/event/ufc-fight-night-st-louis-january-14-2018?f1=Jeremy-Stephens&f2=Dooho-Choi');
 
     var $ = cheerio.load(res.getBody());
-    var fianlresultJson =new Array()
+    var fianlresultJson = new Array()
     var index = 1;
     $('.fight').each(function () {
         var fighter_image1 = $(this).children().children().children('img').attr('src');
@@ -211,7 +212,6 @@ router.get('/fightcard_detail', function (req, _response, next) {
     _response.json({result: fianlresultJson});
 
 });
-
 
 
 router.get('/fightcard', function (req, _response, next) {
