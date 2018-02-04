64c8e8c9b0132c2da869615e5bdb7108adededdd
고경준 <kyungjoon.go@gmail.com>
2018-02-05 08:27:46 +0900

    asdfsdfsdfasdfsfsadfasdf

--- routes/memcard_route.js
+++ routes/memcard_route.js
@@ -9,6 +9,52 @@ var querystring = require('querystring');
 var striptags = require('striptags');
 var prettyjson = require('prettyjson');
 
+//######################
+
+router.get('/memcard_search/', function (req, last_response, next) {
+
+    var search_word = req.query.search_word;
+
+    var res = syncRequest('post', 'http://mecard.sonokong.co.kr/Card/CardList?cardname=y&skillname=y&searchString=+' + encodeURI(search_word));
+
+
+    var $ = cheerio.load(res.getBody());
+
+    var resultJson = new Array()
+
+    var count = '';
+    $('.related  >li ').each(function () {
+
+        var image = $(this).children().children().attr('src');
+        var desc = $(this).children().children().attr('alt');
+        var baseUrl = 'http://mecard.sonokong.co.kr'
+        let __imageARr = image.split('?')
+        let __imageARr2 = __imageARr[1].split('=')
+
+        ///Card/CardView?crd_idx=
+        var popupbase = '/Card/CardView?crd_idx='
+
+
+        popupbase = baseUrl + popupbase + __imageARr2[1]
+
+
+        resultJson.push({
+
+            image: baseUrl + image,
+            desc: desc,
+            href: __imageARr2[1],
+            popup: popupbase
+
+        })
+
+    });
+
+
+
+    last_response.json(resultJson)
+
+});
+
 
 router.get('/memcard_detail/', function (req, last_response, next) {
 
@@ -50,10 +96,12 @@ router.get('/memcard/', function (req, last_response, next) {
 
     var page = req.query.page;
     /*var table_name = req.query.table_name;
-    var search_word = req.query.search_word*/
+    */
+
+    //var search_word = req.query.search_word
 
 
-    var res = syncRequest('post', 'http://mecard.sonokong.co.kr/Card/CardList?cardname=y&skillname=y&page='+ page);
+    var res = syncRequest('post', 'http://mecard.sonokong.co.kr/Card/CardList?cardname=y&skillname=y&page='+ page );
 
 
 
